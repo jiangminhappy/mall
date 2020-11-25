f2c5417544367001146a578c1a5faa92411d0925
jiangminhappy <2154005597@qq.com>
2020-11-25 20:16:19 +0800

    首页基本完成

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tab-control" >
+    <div v-for="(item, index) in title" 
+         :key="item.title" 
+         class="item"
+        :class="{active : index === curIndex}"
+        @click="handleClick(index)"
+        >
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    title: Array
+  },
+  data() {
+    return {
+      curIndex: 0
+    }
+  },
+  methods: {
+    handleClick(index) {
+      this.curIndex = index;
+      this.$emit('tabClick', index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+    background-color: #fff;
+    margin-bottom: 10px;
+    z-index: 10;
+  }
+
+  .item {
+    flex: 1;
+  }
+
+  .active {
+    color: #ff5777;
+    border-bottom: 3px solid #ff5777;
+
+  }
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,35 +8,94 @@
     <home-swiper :banners="banners" />
     <!-- 推荐 -->
     <recommend-view :recommend="recommend" />
+    <!-- 特色的区域 -->
+    <feature-view />
+    <!-- 分类的区域 -->
+    <tab-control 
+      class="tab-control" 
+      :title="['流行', '新款', '精选']" 
+      @tabClick="tabClick" 
+    />
+    <goods-list :goods="showGoods" />
   </div>
 </template>
 
 <script>
+import { getHomeMultidata, getHomeGoods } from 'network/home'
+
 import NavBar from 'components/common/navbar/NavBar'
-import { getHomeMultidata } from 'network/home'
+import TabControl from 'components/content/tabControl/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
 
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView.vue'
 
 export default {
   name: 'Home',
   components: {
     NavBar,
+    TabControl,
+    GoodsList,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
   },
   data() {
     return {
       banners: [],
-      recommend: []
+      recommend: [],
+      goods: {
+        'pop': { page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': { page: 0, list: []}
+      },
+      currentType: 'pop'
+    }
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list
     }
   },
   created() {
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list;
-      this.recommend = res.data.recommend.list;
-      console.log(this.recommend)
-})
+    // 请求轮播图的数据
+    this.getHomeMultidata();
+    // 请求详情的数据
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+  },
+  methods: {
+    // 网络请求
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      })
+    },
+    // 事件监听
+    tabClick(index) {
+      // console.log(index)
+      switch(index) {
+        case 0:
+          this.currentType = 'pop'
+          break
+        case 1:
+          this.currentType = 'new'
+          break
+        case 2:
+          this.currentType = 'sell'
+          break
+      }
+    }
   }
 }
 </script>
@@ -45,5 +104,18 @@ export default {
   .home-nav {
     background-color: #ff8198;
     color: #FFF;
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    z-index: 9;
+  }
+  #home {
+    padding-top: 44px;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px
   }
 </style>
